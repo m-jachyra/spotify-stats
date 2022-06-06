5f28d8bbf2d8d32c6cc9c4a0afd73fd2a03679d5
m-jachyra <marek792783@gmail.com>
2022-06-06 19:21:22 +0200

    Move to Docker

--- dags/helpers/mongo_helpers.py
+++ dags/helpers/mongo_helpers.py
@@ -2,7 +2,7 @@ def get_database():
     from pymongo import MongoClient
     import pymongo
 
-    CONNECTION_STRING = 'mongodb://root:example@localhost:27017'
+    CONNECTION_STRING = 'mongodb://root:example@mongo:27017'
 
     from pymongo import MongoClient
     client = MongoClient(CONNECTION_STRING)
